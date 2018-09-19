c832f729548ab63c3376d7d1a8c45d030d3e71a4
liutao <taoliuh@gmail.com>
2018-09-19 20:39:28 +0800

    first commit

---
+++ app/src/main/java/me/sonaive/kaka/MainActivity.java
@@ -0,0 +1,14 @@
+package me.sonaive.kaka;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
